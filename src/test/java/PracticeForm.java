565388752c09dc288921cda7699ba887233388f6
My Name <belkatan@yandex.ru>
2021-03-20 20:30:25 +0300

    update

--- src/test/java/PracticeForm.java
+++ src/test/java/PracticeForm.java
@@ -1,19 +1,20 @@
 import com.codeborne.selenide.Configuration;
-import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 
 import java.io.File;
-import static com.codeborne.selenide.Selectors.*;
-import static com.codeborne.selenide.Selenide.*;
+
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
 
 public class PracticeForm {
 
     @BeforeAll
     static void setup() {
         Configuration.startMaximized = true;
-    }
+        }
 
     @Test
     void selenideSearchTest() {
